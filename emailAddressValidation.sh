98856543e012e91756f58e47f7895ad2f7229de2
Preranashinde <preranapshinde3009@gmail.com>
2020-03-28 18:15:19 -0300

    Validate the mandatory part of email address

---
+++ emailAddressValidation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+read -p "Enter your email address:" emailAddress
+toValidateEmailId="^[a-zA-Z]{1,}([.]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2,3})?$"
+if [[ $emailAddress =~ $toValidateEmailId ]]
+then
+   echo Email Address is Valid
+else
+   echo Email Address is Invalid
+fi
+
